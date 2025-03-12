8c8ffcf15dc065ff863d9ecc6a32f15fad462326
romain mornet <romainmornet@macbookpro.home>
2025-03-12 17:11:16 +0100

    update performance 4

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  poweredByHeader: false,
-  experimental: {
-    optimizeCss: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
